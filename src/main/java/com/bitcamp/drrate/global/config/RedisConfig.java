6d434ce17e4ca1f9b36261a576b8e5c311fe8307
YoungSu Oh <110619825+YoungSuOh@users.noreply.github.com>
2024-12-20 11:55:14 +0900

    release : Redis 클러스터 연결 설정

--- src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
+++ src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
@@ -1,16 +1,38 @@
 package com.bitcamp.drrate.global.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.List;
+
 @Configuration
 public class RedisConfig {
+
+    @Value("${info.redis.clusterNode}")
+    private List<String> clusterNodes;
+
+    @Bean
+    @Primary
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterNodes.forEach(node -> {
+            String[] parts = node.split(":");
+            RedisNode redisNode = new RedisNode(parts[0], Integer.parseInt(parts[1]));
+            clusterConfiguration.addClusterNode(redisNode);
+        });
+        return new LettuceConnectionFactory(clusterConfiguration);
+    }
+
     @Bean
     @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
