22974abaa8048fe1cb03e2e17420b0e07a546318
unknown <paul9119298@gmail.com>
2024-12-25 17:28:57 +0900

    fix : kafka topic 만들어지기 전 consume problem solve

--- src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
+++ src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
@@ -15,27 +15,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.util.List;
 
+
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.data.redis.cluster.nodes[0]}")
-    private String node1;
-
-    @Value("${spring.data.redis.cluster.nodes[1]}")
-    private String node2;
-
-    @Value("${spring.data.redis.cluster.nodes[2]}")
-    private String node3;
-
-    @Bean
-    @Primary
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
-        clusterConfiguration.addClusterNode(new RedisNode(node1.split(":")[0], Integer.parseInt(node1.split(":")[1])));
-        clusterConfiguration.addClusterNode(new RedisNode(node2.split(":")[0], Integer.parseInt(node2.split(":")[1])));
-        clusterConfiguration.addClusterNode(new RedisNode(node3.split(":")[0], Integer.parseInt(node3.split(":")[1])));
-        return new LettuceConnectionFactory(clusterConfiguration);
-    }
     @Bean
     @Primary
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
