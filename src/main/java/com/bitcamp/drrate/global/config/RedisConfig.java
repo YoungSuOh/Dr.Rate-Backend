d73dff15ff31d7e01f0cee2b429cbbd951f57450
Young Su Oh <110619825+YoungSuOh@users.noreply.github.com>
2024-12-25 17:49:58 +0900

    release 브랜치에 dev 병합 수정 #2

--- src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
+++ src/main/java/com/bitcamp/drrate/global/config/RedisConfig.java
@@ -18,20 +18,22 @@ import java.util.List;
 
 @Configuration
 public class RedisConfig {
+     @Value("${spring.data.redis.cluster.nodes[0]}")
+    private String node1;
+
+    @Value("${spring.data.redis.cluster.nodes[1]}")
+    private String node2;
+
+    @Value("${spring.data.redis.cluster.nodes[2]}")
+    private String node3;
+
     @Bean
     @Primary
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        // Key와 Hash Key는 String 직렬화
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-
-        // Value와 Hash Value는 JSON 직렬화
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        return template;
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+        clusterConfiguration.addClusterNode(new RedisNode(node1.split(":")[0], Integer.parseInt(node1.split(":")[1])));
+        clusterConfiguration.addClusterNode(new RedisNode(node2.split(":")[0], Integer.parseInt(node2.split(":")[1])));
+        clusterConfiguration.addClusterNode(new RedisNode(node3.split(":")[0], Integer.parseInt(node3.split(":")[1])));
+        return new LettuceConnectionFactory(clusterConfiguration);
     }
 }
