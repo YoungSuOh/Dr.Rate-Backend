b1f6c6d27a69c6057386082d980b60048fb84138
Young Su Oh <110619825+YoungSuOh@users.noreply.github.com>
2024-12-30 16:42:44 +0900

    로드밸런서 헬스체크 url  설정

--- src/main/java/com/bitcamp/drrate/global/config/SecurityConfig.java
+++ src/main/java/com/bitcamp/drrate/global/config/SecurityConfig.java
@@ -107,6 +107,7 @@ public class SecurityConfig {
         // === 경로별 접근 권한 ===
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                        "/api/health/**",
                         "/api/signIn/**",
                         "/api/signUp/**",
                         "/ws/**",
